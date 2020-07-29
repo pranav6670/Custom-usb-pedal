bcd47fa8d8eb8086a9ea70af8e08bcc5a87be4b2
Pranav  Natekar <pranavnat24@gmail.com>
2020-07-29 18:55:42 +0530

    Adiing main Js

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pyfirmata
 import time
 from pynput.keyboard import Key, Controller
 import serial.tools.list_ports
+import eel
 
-pin = 7
+# eel.init('web')
 
 keyboard = Controller()
 ports = list(serial.tools.list_ports.comports())
+
 
 def detect_ports():
     arduino_ports = []
@@ -15,30 +17,37 @@ def detect_ports():
             arduino_ports.append(p)
     if len(arduino_ports) == 0:
         print("no Arduino board detected")
-    if len(arduino_ports) > 1:
-        print('Multiple Arduinos found - using the first')
-    else:
-        print("Arduino board detected")
 
     return arduino_ports
 
+
+def execute(board, pin):
+    it = pyfirmata.util.Iterator(board)
+    it.start()
+
+    board.digital[pin].mode = pyfirmata.INPUT
+
+    while True:
+        sw = board.digital[pin].read()
+        if sw is True:
+            board.digital[13].write(1)
+            keyboard.press('b')
+            print("Pressed")
+        else:
+            board.digital[13].write(0)
+            keyboard.release('b')
+        time.sleep(0.1)
+
+
 a = detect_ports()
-print(a[0].device)
+
+if a:
+    print("Board found")
+    print(a[0].device)
+else:
+    print("No arduino found")
 
 board = pyfirmata.Arduino(str(a[0].device))
+execute(board, 7)
 
-it = pyfirmata.util.Iterator(board)
-it.start()
-
-board.digital[pin].mode = pyfirmata.INPUT
-
-while True:
-    sw = board.digital[pin].read()
-    if sw is True:
-        board.digital[13].write(1)
-        keyboard.press('b')
-        print("Pressed")
-    else:
-        board.digital[13].write(0)
-        keyboard.release('b')
-    time.sleep(0.1)
+# eel.start('index.html', size=(400, 400))
